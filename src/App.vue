931950db105e18823601ea0af5cba438530b1c3a
Sergey Meshchankin <smeshchankin@gmail.com>
2020-10-28 00:58:10 +0200

    [TD-000] Init: dummy Vue project

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>Todo Application</h1>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
